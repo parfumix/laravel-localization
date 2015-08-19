01de230dd498431d7fb1b586ef121ce4cc0fd6d1
Radu <radu.bordei89@gmail.com>
2015-08-19 18:43:28 +0300

    adding browser detector bussines logic.

--- src/Detectors/Browser.php
+++ src/Detectors/Browser.php
@@ -3,11 +3,35 @@
 namespace Localization\Detectors;
 
 use Localization\Detectable;
+use Localization;
 
 class Browser implements Detectable {
 
     public function detect($request) {
+        $acceptLanguages = $request->header('accept-language');
 
+        if( isset($acceptLanguages) && strlen($acceptLanguages) > 1 )  {
+            $acceptLanguages = explode(",", $acceptLanguages);
+
+            $lang = [];
+            foreach ($acceptLanguages as $language) {
+                if( preg_match( "/(.*);q=([0-1]{0,1}.\\d{0,4})/i", $language, $matches ) )
+                    $lang[$matches[1]] = (float)$matches[2];
+                else
+                    $lang[$language] = 1.0;
+            }
+
+            $qval = 0.0;
+            foreach ($lang as $key => $value) {
+                if ($value > $qval) {
+                    $qval = (float)$value;
+                    $deflang = $key;
+                }
+            }
+        }
+
+        return Localization\format_locale($deflang)
+            ->getLanguage();
     }
 
 }
