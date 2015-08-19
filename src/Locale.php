8220cda6ea4ab04a748499f1c2a731aa716744ce
Radu <radu.bordei89@gmail.com>
2015-08-19 18:48:20 +0300

    adding comments.

--- src/Locale.php
+++ src/Locale.php
@@ -17,53 +17,112 @@ class Locale {
     protected $request;
 
 
+    /**
+     * Add new locales .
+     *
+     * @param array $locales
+     * @return $this
+     */
     public function addLocales(array $locales = array()) {
         $this->locales = array_merge(array_flip($locales), $this->getLocales());
 
         return $this;
     }
 
+    /**
+     * Set locales .
+     *
+     * @param array $locales
+     * @return $this
+     */
     public function setLocales(array $locales = array()) {
         $this->locales = array_flip($locales);
 
         return $this;
     }
 
+    /**
+     * Get locales .
+     *
+     * @return array
+     */
     public function getLocales() {
         return $this->locales;
     }
 
+    /**
+     * Get locale .
+     *
+     * @param $locale
+     * @return null
+     */
     public function getLocale($locale) {
         return isset($this->locales[$locale]) ? $locale : null;
     }
 
 
+    /**
+     * Set default locale .
+     *
+     * @param $locale
+     * @return $this
+     */
     public function setDefault($locale) {
         $this->default = $locale;
 
         return $this;
     }
 
+    /**
+     * Get default locale .
+     *
+     * @return mixed
+     */
     public function getDefault() {
         return $this->default;
     }
 
+
+    /**
+     * Set http request .
+     *
+     * @param $request
+     * @return $this
+     */
     public function setRequest($request) {
         $this->request = $request;
 
         return $this;
     }
 
+    /**
+     * Get http request .
+     *
+     * @return mixed
+     */
     public function getRequest() {
         return $this->request;
     }
 
 
+    /**
+     * Check if is valid locale .
+     *
+     * @param $locale
+     * @return null
+     */
     public function isValid($locale) {
         return $this->getLocale($locale);
     }
 
 
+    /**
+     * Set current locale .
+     *
+     * @param $locale
+     * @return $this
+     * @throws LocaleException
+     */
     public function setCurrentLocale($locale) {
         if(! $this->isValid($locale))
             throw new LocaleException(_('Unsupported locale'));
@@ -73,22 +132,44 @@ class Locale {
         return $this;
     }
 
-    public function getActiveLocale() {
+    /**
+     * Get current locale .
+     *
+     * @return mixed
+     */
+    public function getCurrentLocale() {
         return $this->currentLocale;
     }
 
-
+    /**
+     * Set fallback .
+     *
+     * @param $locale
+     * @return $this
+     */
     public function setFallBack($locale) {
         $this->fallBack = $locale;
 
         return $this;
     }
 
+    /**
+     * Get fallback .
+     *
+     * @return mixed
+     */
     public function getFallBack() {
         return $this->fallBack;
     }
 
 
+    /**
+     * Detect locale .
+     *
+     * @param callable $onSuccess
+     * @return $this
+     * @throws LocaleException
+     */
     public function detect(\Closure $onSuccess = null) {
         $detectors = $this->getDetectors();
         $locale    = null;
@@ -121,12 +202,23 @@ class Locale {
         return $this;
     }
 
+    /**
+     * Set detectors .
+     *
+     * @param array $detectors
+     * @return $this
+     */
     public function setDetectors(array $detectors = array()) {
         $this->detectors = $detectors;
 
         return $this;
     }
 
+    /**
+     * Get detectors .
+     *
+     * @return array
+     */
     public function getDetectors() {
         return $this->detectors;
     }
