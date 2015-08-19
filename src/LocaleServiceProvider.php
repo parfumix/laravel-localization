f1b3c1f549141029e3cfe81bd521f72a564bbcec
Radu <radu.bordei89@gmail.com>
2015-08-19 17:00:30 +0300

    register configurations.

--- src/LocaleServiceProvider.php
+++ src/LocaleServiceProvider.php
@@ -2,24 +2,36 @@
 
 namespace Localization;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Yaml\Yaml;
 
 class LocaleServiceProvider extends Serviceprovider {
 
-    public function boot() {
-        
-    }
+    public function boot(Request $request) {
+        $this->loadConfiguration();
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register() {
-        $this->app->share('locale', function() {
-            return new Locale;
+        $this->app->bind('laravel-locale', function() use($request) {
+            return (new Locale)
+                ->setRequest($request)
+                ->setLocales(config('laravel-locale.locales'))
+                ->setDefault(config('laravel-locale.fallback'));
         });
     }
 
+    public function register() {}
+
+    #@todo adding helper which will do all that  .
+    protected function loadConfiguration() {
+        $array = Yaml::parse(file_get_contents(
+            __DIR__ . '/../configuration/general.yaml'
+        ));
+
+        $config = $this->app['config']->get('laravel-locale', []);
+
+        $this->app['config']->set('laravel-locale', array_merge($array, $config));
+
+        return $this;
+    }
 }
 
