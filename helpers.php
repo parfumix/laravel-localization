b12d081fe580cece6e56d2dcdf866d121c002bd5
Radu <radu.bordei89@gmail.com>
2015-08-25 14:41:16 +0300

    get locale for specific detector.

--- helpers.php
+++ helpers.php
@@ -28,8 +28,26 @@ function get_default_locale() {
  * @return mixed
  */
 function get_active_locale() {
-    return app('laravel-locale')
+    $currentLocale = app('laravel-locale')
         ->getCurrentLocale();
+
+    return $currentLocale;
+}
+
+/**
+ * Get locale for specific detector .
+ *
+ * @param $detector
+ * @return mixed
+ */
+function get_detector_locale($detector) {
+    $request = app('request');
+
+    $detector = app('laravel-locale')
+        ->getDetector($detector);
+
+    return $detector
+        ->detect($request);
 }
 
 /**
